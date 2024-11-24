fbc91d40c14bb33e0c5e28437c11858b1a22d182
GabrielMaurici0 <mauriciogabriel332@gmail.com>
2024-11-24 15:27:55 -0300

    Update: Adicionado comentários e adicionando novas funcionalidades

--- teste.py
+++ teste.py
@@ -1,34 +1,59 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 import json
 
+
+#abre arquivo json
 with open('values.json', 'r') as arquivo:
     dados = json.load(arquivo)
 
+#abre o navegador
 nav = webdriver.Chrome()
 
+#criação obj ActionChains
+action = ActionChains(nav)
+
+#após abrir o navegador vai para a url
 nav.get(dados['url'])
 
-time.sleep(3)
+#espera a pagina carregar
+time.sleep(2)
 
+#busca os botões e campos 
 usucod = nav.find_element(by=By.NAME, value="_USUCODC")
 ususen = nav.find_element(by=By.NAME, value="_USUSEN")
 btncon = nav.find_element(by=By.NAME, value="BUTTON1")
 
-time.sleep(3)
-
-
+#limpa o campo e preenche em seguida
 usucod.clear()
 usucod.send_keys(dados['usucod'])
 
-time.sleep(5)
-
+#preenche o campo 
 ususen.send_keys(dados['ususen'])
 
-# time.sleep(2)
+#clica no botão para prosseguir
+btncon.click()
 
-#btncon.click()
+#aguardar carregar a pagina
+time.sleep(2)
 
+#busca o elemento
+btnsim = nav.find_element(by=By.NAME, value="BUTTON3")
 
-time.sleep(50000)
+#clica no elemento
+btnsim.click()
+
+#busca o elemento que contenha 'Cadastro' no seu texto e em seguida move o mouse para sua posição 
+#e faz a função "hover"
+elecad = nav.find_element(by=By.XPATH, value="//a[contains(text(), 'Cadastro')]")
+action.move_to_element(elecad).perform()
+
+time.sleep(2)
+
+#bucas pelo elemento que tenha "usuario" e clica no elemento
+eleusu = nav.find_element(by=By.XPATH, value="//a[contains(text(), 'Usuário')]")
+eleusu.click()
+
+nav.quit()
